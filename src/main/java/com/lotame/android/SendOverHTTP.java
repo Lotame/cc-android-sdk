ac3098ab20fc6274f1408eeb2730dfb54727cb53
Kevin O'Connor <kevin@lotame.com>
2021-06-30 18:02:49 -0400

    Add support for requesting the panorama id from the audience extraction api.

--- src/main/java/com/lotame/android/SendOverHTTP.java
+++ src/main/java/com/lotame/android/SendOverHTTP.java
@@ -13,7 +13,7 @@ import java.util.Map;
 /**
  * The MIT License (MIT)
  *
- *  Copyright (c) 2015 Lotame
+ *  Copyright (c) 2021 Lotame
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
